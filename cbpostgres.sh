b7cae8a51e2b82c94929d90ce457235a2d33ace5
svenugopal333 <47510467+svenugopal333@users.noreply.github.com>
2019-02-13 14:22:18 +1100

    Update cbpostgres.sh

--- cbpostgres.sh
+++ cbpostgres.sh
@@ -1,15 +1,45 @@
 #!/usr/bin/env bash
-pgserver="" #eg: cbreakpsql.postgres.database.azure.com
-pgserverport=""  # 5432
-pgserverusername="" #psqladmin@postgresserver
-pgserverpassword="" # MyserverP@ssword
+: '
+This script is meant for integrating the SSL Enabled Azure Postgres into Cloudbreak as its backend
+Pre-requisites for executing this script
+1. SSL cert for accessing the Azure postgres server must be placed under /var/lib/cloudbreak-deployment/
+2. cert name has to renamed as database.crt  ( Eg: /var/lib/cloudbreak-deployment/database.crt )
+3. Azure Postgres server details must be specified 
+'
+
+# Installing the Postgres Client
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-2.noarch.rpm  
+sudo yum install -y postgresql10 
+
+# Getting the Postgres Server details
+pgserver="cbreakpsql.postgres.database.azure.com" #eg: cbreakpsql.postgres.database.azure.com
+pgserverport="5432"  # 5432
+pgserverusername="psqladmin@postgresserver" # psqladmin@postgresserver
+pgserverpassword="MyserverP@ssword" # MyserverP@ssword
+
+# Set the Environment variables
+export DATABASE_HOST=$pgserver
+export DATABASE_PORT=$pgserverport
+export DATABASE_USERNAME=$pgserverusername
+export DATABASE_PASSWORD=$pgserverpassword
+
+# Checking the required DB created i.e cbdb,periscopedb, uaadb and the cert exists in /var/lib/cloudbreak-deployment/cert/database.crt for SSL enabled Postgres Login
+if [ ! -z "$pgserver" ] && [ ! -z "$pgserverport" ] && [ ! -z "$pgserverusername" ] && [ ! -z "$pgserverpassword" ] && [ -f /var/lib/cloudbreak-deployment/certs/database.crt ]; then
+    echo "Checking the DB connectivity"
 
 
+else
+    echo "Please enter all these details Postgres_server_hostname/Port/Username/Pwd and cert exists in /var/lib/cloudbreak-deployment/certs/database.crt"
+fi
+
+
+
+# Updating the Profile file
 cat >> /var/lib/cloudbreak-deployment/Profile << END
 export DATABASE_HOST=$pgserver
-export DATABASE_PORT=5432
-export DATABASE_USERNAME=psqladmin@cbreakpsql
-export DATABASE_PASSWORD=Hadoop-123
+export DATABASE_PORT=$pgserverport
+export DATABASE_USERNAME=$pgserverusername
+export DATABASE_PASSWORD=$pgserverpassword
 export CB_DB_PORT_5432_TCP_ADDR=$DATABASE_HOST
 export CB_DB_PORT_5432_TCP_PORT=$DATABASE_PORT
 export CB_DB_ENV_USER=$DATABASE_USERNAME
@@ -27,3 +57,6 @@ export IDENTITY_DB_USER=$DATABASE_USERNAME
 export IDENTITY_DB_PASS=$DATABASE_PASSWORD
 export IDENTITY_DB_NAME=uaadb
 END
+
+# restarting the cbd using the command "cbd restart"
+cd /var/lib/cloudbreak-deployment/ && cbd restart
