4ebe91e945cb0104a8e0166fe2a4ed1920be20cf
svenugopal333 <47510467+svenugopal333@users.noreply.github.com>
2019-02-13 16:39:24 +1100

    Update psql4clusters.sh

--- psql4clusters.sh
+++ psql4clusters.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+pgserver=" "
+pghivedbname=" "
+pgusername=" "
+pgpwd=" "
+
+cb database create postgres --name hivedb --type HIVE --url jdbc:postgresql://$pgserver:5432/$pghivedbname --db-username $pgusername --db-password $pgpwd
