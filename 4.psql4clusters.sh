3fc37f9044bfd91b46e158fc352e41fc1ad66b70
svenugopal333 <47510467+svenugopal333@users.noreply.github.com>
2019-02-14 18:10:04 +1100

    Update 4.psql4clusters.sh

--- 4.psql4clusters.sh
+++ 4.psql4clusters.sh
@@ -2,12 +2,15 @@
 
 : '
 Create database for  Ambari, Druid, Hive, Oozie, Ranger, Registry, Superset
-'
-
 pgserver=" "
 pghivedbname=" "
 pgusername=" "
 pgpwd=" "
+
+'
+# Installing the Postgres Client
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-redhat10-10-2.noarch.rpm  
+sudo yum install -y postgresql10 
 
 
 sudo -i -u postgres psql -c 'CREATE DATABASE ambari'
@@ -19,5 +22,7 @@ sudo -i -u postgres psql -c 'CREATE DATABASE registry'
 sudo -i -u postgres psql -c 'CREATE DATABASE superset'
 
 
+cb database create postgres --name hive  --type HIVE --url jdbc:postgresql://testsrj.field.hortonworks.com:5432/hive?ssl=true --db-username cbadmin --db-password Hadoop-123
 
-cb database create postgres --name hive --type HIVE --url jdbc:postgresql://$pgserver:5432/$pghivedbname --db-username $pgusername --db-password $pgpwd
+
+cb database create postgres --name hive --type HIVE --url jdbc:postgresql://$pgserver:5432/$pghivedbname?ssl=true --db-username $pgusername --db-password $pgpwd
