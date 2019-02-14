1605447a890b5314ef5ba37084bc3a500cbc7a66
svenugopal333 <47510467+svenugopal333@users.noreply.github.com>
2019-02-14 17:55:17 +1100

    Update 4.psql4clusters.sh

--- 4.psql4clusters.sh
+++ 4.psql4clusters.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
+: '
+Create database for  Ambari, Druid, Hive, Oozie, Ranger, Registry, Superset
+'
+
 pgserver=" "
 pghivedbname=" "
 pgusername=" "
 pgpwd=" "
 
-cb database create postgres --name hivedb --type HIVE --url jdbc:postgresql://$pgserver:5432/$pghivedbname --db-username $pgusername --db-password $pgpwd
+
+sudo -i -u postgres psql -c 'CREATE DATABASE ambari'
+sudo -i -u postgres psql -c 'CREATE DATABASE druid'
+sudo -i -u postgres psql -c 'CREATE DATABASE hive'
+sudo -i -u postgres psql -c 'CREATE DATABASE oozie'
+sudo -i -u postgres psql -c 'CREATE DATABASE ranger'
+sudo -i -u postgres psql -c 'CREATE DATABASE registry'
+sudo -i -u postgres psql -c 'CREATE DATABASE superset'
+
+
+
+cb database create postgres --name hive --type HIVE --url jdbc:postgresql://$pgserver:5432/$pghivedbname --db-username $pgusername --db-password $pgpwd
