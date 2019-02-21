36381178d634a8f3a975bcf7c98b281bb3dd8241
svenugopal333 <47510467+svenugopal333@users.noreply.github.com>
2019-02-21 12:03:05 +1100

    Update 4.psql4clusters.sh

--- 4.psql4clusters.sh
+++ 4.psql4clusters.sh
@@ -7,17 +7,18 @@ This script helps in integrating the Postgres databases (for HDP/HDF clusters) t
 It requires the following as input:
 
 1. Postgres DB server FQDN
-2. DB 
+2. DB Username and password
+3. Cloudbreak deployer VM fqdn along with the username and password for Cloudbreak WebUI
 
 '
-pgserver=" "
-pgusername=" "
-pgpwd=" "
+pgserver="cbpostgres.postgres.database.azure.com"
+pgusername="cbpsqladmin@cbpostgres"
+pgpwd="Hadoop-12345"
+cburl="https://52.237.208.167"
+cbuser="cbadmin@example.com "
+cbpasswd="Hadoop-123"
 
-
-cb configure --server https://52.237.208.167 --username cbadmin@example.com --password "Hadoop-123"
-cb database create postgres --name myambari --type AMBARI --url jdbc:postgresql://cbpostgres.postgres.database.azure.com:5432/ambari?ssl=true  --db-username cbpsqladmin@cbpostgres --db-password Hadoop-123
-cb database create postgres --name myhive --type HIVE --url jdbc:postgresql://cbpostgres.postgres.database.azure.com:5432/hive?ssl=true  --db-username cbpsqladmin@cbpostgres --db-password Hadoop-123
-cb database create postgres --name myranger --type RANGER --url jdbc:postgresql://cbpostgres.postgres.database.azure.com:5432/rangerdb?ssl=true  --db-username cbpsqladmin@cbpostgres --db-password Hadoop-123
-cb database create postgres --name hive  --type HIVE --url jdbc:postgresql://testsrj.field.hortonworks.com:5432/hive?ssl=true --db-username cbadmin --db-password Hadoop-123
-cb database create postgres --name hive --type HIVE --url jdbc:postgresql://$pgserver:5432/$pghivedbname?ssl=true --db-username $pgusername --db-password $pgpwd
+cb configure --server $cburl --username $cbuser --password $cbpasswd
+cb database create postgres --name rangerdb --type AMBARI --url jdbc:postgresql://$pgserver:5432/ambari?ssl=true  --db-username $pgusername --db-password $pgpwd
+cb database create postgres --name hivedb --type HIVE --url jdbc:postgresql://$pgserver:5432/hive?ssl=true --db-username $pgusername --db-password $pgpwd
+cb database create postgres --name rangerdb --type RANGERDB --url jdbc:postgresql://$pgserver:5432/rangerdb?ssl=true --db-username $pgusername --db-password $pgpwd
