2952959d02a339ddafe3e9bb09aa3bf7a444cd27
svenugopal333 <47510467+svenugopal333@users.noreply.github.com>
2019-02-21 12:17:54 +1100

    Update 4.psql4clusters.sh

--- 4.psql4clusters.sh
+++ 4.psql4clusters.sh
@@ -17,7 +17,7 @@ pgpwd="Hadoop-12345"
 cburl="https://52.237.208.167"
 cbuser="cbadmin@example.com "
 cbpasswd="Hadoop-123"
-
+curl -Ls https://s3-us-west-2.amazonaws.com/cb-cli/cb-cli_2.9.0_Linux_x86_64.tgz | sudo tar -xz -C /bin cb && chmod +x /bin/cb
 cb configure --server $cburl --username $cbuser --password $cbpasswd
 cb database create postgres --name rangerdb --type AMBARI --url jdbc:postgresql://$pgserver:5432/ambari?ssl=true  --db-username $pgusername --db-password $pgpwd
 cb database create postgres --name hivedb --type HIVE --url jdbc:postgresql://$pgserver:5432/hive?ssl=true --db-username $pgusername --db-password $pgpwd
