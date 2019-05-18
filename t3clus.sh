d81bc516491f4493b51222706fec8cf5294d9af2
Svenugopal <47510467+svenugopal333@users.noreply.github.com>
2019-05-18 15:48:02 +1000

    Update t3clus.sh

--- t3clus.sh
+++ t3clus.sh
@@ -40,24 +40,53 @@ Components required:
         # "http://artifactory/cb"
         # "https://cbvm.com"
           # "http://artifactory/jq"
-Blueprint=$3  # "http://artifactory/var/lib/cloudbreak-deployment/hdpbp.json"         # "http://artifactory/hdpblueprint.json"
-arm_url=$4    # "http://artifactory/var/lib/cloudbreak-deployment/t3arm.json"        # "http://artifactory/t3-arm-template.json"
-cbusername=$1 # "cbadmin@example.com"  # This variable will be parametrized in the future release
-cbpwd=$2      # "some secure pwd from DevOps "   #This variable will be parametrized in the future release
+ # "http://artifactory/var/lib/cloudbreak-deployment/hdpbp.json"         # "http://artifactory/hdpblueprint.json"
 
 # Mandatory parameters for this script
-cb_web_url="https://$(ip add | grep 'state UP' -A2 | head -n3 | awk '{print $2}' | cut -f1 -d'/' | tail -n1)"
-cbutilpath=$1
-jqurl=$2
-cbusrname=$3
-cbpwd=$4
-cbjqtemplate=$5
-cbjqtemplatepath="/tmp/.cbjqtmp.json"
-cbinputtemplate=$6
-cbinputtemplatepath="/tmp/.cbinputtemplate.json"
-tstmp=$(date "+%Y%m%d%H%M%S")
 
-start_script_0()
+# CB vm related details
+
+    #cb_web_url="https://$(ip add | grep 'state UP' -A2 | head -n3 | awk '{print $2}' | cut -f1 -d'/' | tail -n1)"
+cb_web_url="https://$1"  # $1 Cloudbreak VM IP address
+cbutilpath="http://172.26.201.244/cb"   #  $2 CB file url from artifactory
+jqurl="http://172.26.201.244/jq"   #  $3 JQ file url from artifactory
+cbusrname=$4
+cbpwd=$5
+
+#input files 
+tstmp=$(date "+%Y%m%d%H%M%S")
+clusname="hdp$tstmp"  #"Name of the cluster"
+
+cbjqtemplate="http://172.26.201.244/jqtemplate" #$6
+cbjqtemplatepath="/tmp/.cbjqtmp_$tstmp.json"
+
+cbinputtemplate="http://172.26.201.244/inptemplate" #$7
+cbinputtemplatepath="/tmp/.cbinputtemplate_$tstmp.json"
+
+cb_finaltemplate="/tmp/.cbfinaltemplate_$tstmp.json"
+
+#blueprint url
+bp_url="http://172.26.201.244/hdpbp.json" # $8
+bp_path="/tmp/.hdpbp_$tstmp.json"
+
+#======= Template params from Pipeline ================
+s_amb_pwd=$9
+s_krb_princ=${10}
+s_krb_pwd=${11}
+s_kdc_ip=${12}
+s_realm=${13}
+s_hdp_repo=${14}
+s_hdp_util_repo=${15}
+s_vdf_url=${16}
+s_ambari_repo=${17}
+s_subnet=${18}
+s_nwrg=${19}
+s_vnet=${20}
+s_pubkey=${21}
+
+#==================================
+
+start_script_0()   # Testing completed
 {
     if [ -d "/var/log/hwx/" ]; then
         echo `date "+%Y-%m-%d %H:%M:%S : Script Execution started"` >> /var/log/hwx/create_cluster.log
@@ -70,19 +99,19 @@ start_script_0()
  
 add_log() { echo `date "+%Y-%m-%d %H:%M:%S : $1"` >> /var/log/hwx/create_cluster.log; }
 
-exit_script()
+exit_script() # Testing Completed
 {
     add_log "Error !!! $1 !!!"
     add_log "Exiting the script execution"
     exit 1
 }
 
-install_cb_jq_1()
+install_cb_jq_1() # CB Configure need to be tested
 {
     if [[ ! -f /bin/cb ]]; then
-        if [[ $(wget $cbutilpath -O /bin/cb && chmod +x /bin/cb) -eq 0 ]];then
+        if [[ $(wget $cbutilpath -O /bin/cb) -eq 0 ]] && [[ $(chmod +x /bin/cb) -eq 0 ]];then
             add_log "CB utility downloaded successfully"
-            if [[ $(cb configure --server $cb_web_url --username $cbusrname --password $cbpwd) -eq 0 ]];then
+            if [[ $(cb configure --server $cb_web_url --username $cbusrname --password $cbpwd) -eq 0 ]] && [[ $(cb cluster list) -eq 0 ]];then
                 add_log "CB configured succcessfully with Cloudbreak VM"
             else
                 exit_script "Unable to configure CB Utility with Cloudbreak"
@@ -92,7 +121,7 @@ install_cb_jq_1()
         fi
     else
         add_log "CB file already exists"
-        if [[ $(cb configure --server $cb_web_url --username $cbusername --password $cbpwd) -eq 0 ]];then
+        if [[ $(cb configure --server $cb_web_url --username $cbusername --password $cbpwd) -eq 0 ]] && [[ $(cb cluster list) -eq 0 ]];then
             add_log "CB configured succcessfully with Cloudbreak VM"
         else
             exit_script "Unable to configure CB Utility with Cloudbreak"
@@ -101,7 +130,7 @@ install_cb_jq_1()
     add_log "Checking JQ ...."
     if [[ ! -f /bin/jq ]]; then
         add_log "JQ not installed on this machine"
-        if [[ $(wget $jqurl -O /bin/jq && chmod +x /bin/jq) ]]; then
+        if [[ $(wget $jqurl -O /bin/jq) ]] && [[ $(chmod +x /bin/jq) ]]; then
             add_log "Installed JQ version - $(jq --version)"
         else
             exit_script "Error installing JQ"
@@ -111,7 +140,7 @@ install_cb_jq_1()
     fi
 }
 
-get_cb_template_2()
+get_cb_template_2() # Testing Completed
 {
     if [[ $(wget $cbjqtemplate -O $cbjqtemplatepath) -eq 0 ]] && [[ $(wget $cbinputtemplate -O $cbinputtemplatepath) -eq 0 ]]; then
         [[ -s "$cbjqtemplatepath" ]] && add_log "Cloudbreak Template downloaded successfully with contents" || exit_script "Cloudbreak template downloaded as empty"
@@ -122,7 +151,7 @@ get_cb_template_2()
 }
 
 
-update_cb_input_template()
+update_cb_input_template() # Testing completed
 {
     #if [[ $(jq --arg val "$2" --arg key "$1" '.[$key] = $val' /tmp/input.json > /tmp/.mytmp) -eq 0 ]] && [[ $(mv /tmp/.mytmp /tmp/input.json -f) -eq 0 ]]; then
     if [[ $(jq --arg val "$2" --arg key "$1" '.[$key] = $val' $cbinputtemplatepath > /tmp/.mytmp) -eq 0 ]] && [[ $(mv /tmp/.mytmp $cbinputtemplatepath -f) -eq 0 ]]; then
@@ -130,7 +159,6 @@ update_cb_input_template()
             if [[ $2 == "$(cat /tmp/.aftr)" ]]; then
                 add_log "Value of $1 updated in the input template successfully"
                 rm -f /tmp/.aftr
-                #export env_$1=$2
             else
                 exit_script "Unable to update the value of $1 in the input template"
             fi 
@@ -139,71 +167,125 @@ update_cb_input_template()
     fi
 }
 
-#start_script_0
-#cbinputtemplatepath="/tmp/input.json"
-#update_cb_input_template "KDCIP" "1.2.3.4"
 
-
-
-get_pipeline_param()
+get_pipeline_param_3() # Testing completed 
 {
-    export clusname="hdpcluster$tstmp"  #"testclusterhdp"
-    export rgname=$clusname    # eg: "testclusterhdp"
+    add_log "Updating the pipeline parameters to the input file"
+    
+    update_cb_input_template "CLUSNAME" $clusname
+    export HWX_CLUSNAME="$clusname"    # eg: "mycluster name in T3"
+    update_cb_input_template "RGNAME" $clusname
+    export HWX_RGNAME="$clusname"    # eg: "my resource group name in T3"
+
+    update_cb_input_template "AMBARIPWD"  "$1"
+    export HWX_AMBARIPWD="$1"  # $5  # "secure password for ambari through dev ops pipeline"
+    
+    update_cb_input_template "KRBSPN" "$2"
+    export HWX_KRBSPN="$2" # $7     # kerberos service principal eg: ambari/admin@EXAMPLE.COM"
+    
+    update_cb_input_template "KRBPWD" "$3"
+    export HWX_KRBPWD="$3" # $6     # "secure password for kerberos through dev ops pipeline"
+    
+    update_cb_input_template "KDCIP" "$4"
+    export HWX_KDCIP="$4" # $8      # Kerberos KDC IP
+    
+    update_cb_input_template "KADMINIP" "$4" 
+    export HWX_KADMINIP="$4" # $8   # Kerberos Kadmin IP
+    
+    update_cb_input_template "REALM" "$5"
+    export HWX_REALM="$5"  # $9      # Realm name
+    
+    update_cb_input_template "HDPREPOURL" "$6"
+    export HWX_HDPREPOURL="$6"  # HDP repo URL from artifactory eg: "http://public-repo-1.hortonworks.com/HDP/centos7/3.x/updates/3.1.0.0"
+    
+    update_cb_input_template "HDPUTILURL" "$7"
+    export HWX_HDPUTILURL="$7" # HDP util repo url from artifactory eg: "http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.22/repos/centos7"
+    
+    update_cb_input_template "VDFURL" "$8"
+    export HWX_VDFURL="$8"   # Version definition file from artifactory eg : "http://public-repo-1.hortonworks.com/HDP/centos7/3.x/updates/3.1.0.0/HDP-3.1.0.0-78.xml"
+    
+    update_cb_input_template "AMBARIREPOURL" "$9"
+    export HWX_AMBARIREPOURL="$9" # Ambari repo url from artifactory eg: "http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.3.0"
+    
+    update_cb_input_template "SUBNET" "${10}"
+    export HWX_SUBNET="${10}"  # subnet id in Tier 3 to be utilized for the cluster eg:"xaea3sub2703191930"
+    
+    update_cb_input_template "NWRGNAME" "${11}"
+    export HWX_NWRGNAME="${12}" # network resource group name in Tier 3 eg : xaea3RG2703191929
+    
+    update_cb_input_template "VNET" "${12}"
+    export HWX_VNET="${12}"  # Vnet in Tier 3"xaea3vnet2703191930"
+    
+    update_cb_input_template "PUBKEY" "${13}"
+    export HWX_PUBKEY="${13}" # public key for ssh access to the T3 instances getting created 
+}
+
+start_script_0
+install_cb_jq_1
+get_cb_template_2
+# get_pipeline_param "superAmbPWD" "admin/ambari" "superKRBPwd" "kdc.ip.address" "ddep.com" "http://hdprepo.com" "http://hdputilrepo" "http://vdfurl" "http://ambarirepo" "t3subnet" "t3NwRG" "t3vnet" "pubkey_securedone"
+get_pipeline_param_3 "$s_amb_pwd" "$s_krb_princ" "$s_krb_pwd" "$s_kdc_ip" "$s_realm" "$s_hdp_repo" "$s_hdp_util_repo" "$s_vdf_url" "$s_ambari_repo" "$s_subnet" "$s_nwrg" "$s_vnet" "$s_pubkey" 
+
+from_cb_util()  # requires Testing on cb machine
+{
     if [[ $(cb credential list | jq '.[].Name' | cut -f2 -d '"') -eq 0 ]]; then
         add_log "Azure Credential name registered with Cloudbreak - retrieved successfully"
         crname=$(cb credential list | jq '.[].Name' | cut -f2 -d '"')
-        export t3cred=$crname   #"t3ddepcredentials"
+        update_cb_input_template "T3CRED" $crname
+        export HWX_T3CRED=$crname   #"some azure reg name"
     else 
         exit_script "Failed to get the Azure App Key registration name from Cloudbreak"
     fi
-        
     
-    export ambaripwd=$5  # "secure password for ambari through dev ops pipeline"
-    export krbpwd=$6     # "secure password for kerberos through dev ops pipeline"
-    export krbspn=$7     # kerberos service principal eg: ambari/admin@EXAMPLE.COM"
-    export kdcip=$8      # Kerberos KDC IP
-    export kadminip=$8   # Kerberos Kadmin IP
-    export realm=$9      # Realm name
-    export hdprepourl=$10  # HDP repo URL from artifactory eg: "http://public-repo-1.hortonworks.com/HDP/centos7/3.x/updates/3.1.0.0"
-    export hdputilurl=$11 # HDP util repo url from artifactory eg: "http://public-repo-1.hortonworks.com/HDP-UTILS-1.1.0.22/repos/centos7"
-    export vdfurl=$12   # Version definition file from artifactory eg : "http://public-repo-1.hortonworks.com/HDP/centos7/3.x/updates/3.1.0.0/HDP-3.1.0.0-78.xml"
-    export ambarirepourl=$13 # Ambari repo url from artifactory eg: "http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.3.0"
-    export extambdb="myambaridb" #Ambari DB for the cluster
-    export exthivedb="myhivedb"  #Hive DB for the cluster
-    export extrangerdb="myrangerdb"  #ranger db for the cluster
-    export imgcatalog="mycustomcatalog" #custom image catalog registered with cloudbreak
-    export imguuid="c08fb21d-3fa6-46c1-4b41-7a4c2dd40b88"  #custom image UUID registered with cloudbreak
-    export subnet=$14  # subnet id in Tier 3 to be utilized for the cluster eg:"xaea3sub2703191930"
-    export nwrgname=$15 # network resource group name in Tier 3 eg : xaea3RG2703191929
-    export vnet=$16  # Vnet in Tier 3"xaea3vnet2703191930"
-    export pubkey=$17 # public key for ssh access to the T3 instances getting created 
+    if [[ $(cb database list) -eq 0 ]]; then
+        add_log "Database retrieved successfully"
+        export HWX_EXTAMBDB="myambaridb" #Ambari DB for the cluster
+        update_cb_input_template "EXTAMBARIDB" 
+
+        # export HWX_EXTHIVEDB="myhivedb"  #Hive DB for the cluster
+        # update_cb_input_template "EXTHIVEDB" $1
+
+        # export HWX_EXTRANGERDB="myrangerdb"  #ranger db for the cluster
+        # update_cb_input_template "EXTRANGERDB" $2
+    else
+	    exit_script "Unable to retrieve external database name registered with Cloudbreak"
+    fi
+
+
+    if [[ $(cb imagecatalog list | jq '.[].') -eq 0 ]]; then
+        export HWX_IMGCATALOG="mycustomcatalog" #custom image catalog registered with cloudbreak
+        update_cb_input_template
+        add_log ""
+
+        export HWX_IMGUUID="c08fb21d-3fa6-46c1-4b41-7a4c2dd40b88"  #custom image UUID registered with cloudbreak
+        update_cb_input_template
+        add_log ""
+
+    else
+        exit_script "Unable to retrieve Image catalog from Cloudbreak"
+    fi
 }
 
-get_arm()
-{
-    if [[ -f $arm_url ]]; then
-        add_log "ARM template found in /var/lib/cloudbreak-deployment/t3arm.json"
+get_bp() 
+{    
+    if [[ $(wget "$bp_url" -O "$bp_path") -eq 0 ]] && [[ -s "$bp_path" ]]; then
+        add_log "Ambari Blueprint successfully downloaded under $bp_path"
     else
-        exit_script "ARM template not found in /var/lib/cloudbreak-deployment/t3arm.json"
-    fi
-    
-    if [[ -f $Blueprint ]]; then
-        add_log "Blueprint found in /var/lib/cloudbreak-deployment/hdpbp.json"
-    else
-        exit_script "Blueprint not found in /var/lib/cloudbreak-deployment/hdpbp.json"
+        exit_script "Failed to download Ambari Blueprint from Artifactory"
     fi
 }
 
 register_blueprint()
 {
     add_log "Registering custom HDP Blueprint with Cloudbreak"
-    if [[ $(cb blueprint create from-file --name myhdpbp --file $Blueprint) -eq 0 ]]; then
-        add_log "Custom HDP Blueprint registered successfully"
+    if [[ $(cb blueprint create from-file --name $clusname --file $bp_path) -eq 0 ]]; then
+        add_log "Custom HDP Blueprint registered successfully with Cloudbreak"
         add_log "Validating the Blueprint registration using cb command"
-        val=$(cb blueprint list | jq '.[] | {"Name"}' | grep "Name" | cut -f4 -d'"' | grep myhdpbp)
+        val=$(cb blueprint list | jq '.[] | {"Name"}' | grep "Name" | cut -f4 -d'"' | grep $clusname)
         if [[ $val ]]; then
             add_log "Blueprint registered successfully (Blueprint name - $val)"
-            export bpname=$4   #"hdfs-hbase-yarn-grafana-logsearch"
+            export HWX_BPNAME="$clusname"   #"hdfs-hbase-yarn-grafana-logsearch" 
+            update_cb_input_template "BPNAME" "$clusname"
         else
             exit_script "Blueprint validation failed with CB"
         fi
@@ -212,9 +294,18 @@ register_blueprint()
     fi
 }
 
+merge_template()
+{
+    if [[ $(cat $cbinputtemplatepath | jq -f $cbjqtemplatepath > $cb_finaltemplate) -eq 0 ]] && [[ -s $cb_finaltemplate ]]; then
+	    add_log "Successfully merged the input template with Cloudbreak Template"
+    else
+	    exit_script "Failed to merge the input template with Cloudbreak Template"
+    fi
+}
+
 create_cluster()
 {
-    if [[ $(cb cluster create --cli-input-json $arm_url --name myhdp3cluster) -eq 0 ]]; then
+    if [[ $(cb cluster create --cli-input-json $cb_finaltemplate --name $clusname) -eq 0 ]]; then
         add_log "Cluster creation command submitted successfully"
     else
         exit_script "Cluster creation command failed"
@@ -228,27 +319,29 @@ get_cluster_status()
     while [ $status == "Initial" ]
     do
     sleep 10s
-    st=$(cb cluster describe --name myhdp3cluster | jq '.statusReason')
+    st=$(cb cluster describe --name $clusname | jq '.statusReason')
     add_log "Cluster status - $st"
         if [ $st == "Cluster creation finished." ] || [ $st == "Failed" ]; then
-            add_log "Cluster creation completed"
-            fst=$(cb cluster describe --name myhdp3cluster | jq '.cluster.status')
+            fst=$(cb cluster describe --name $clusname | jq '.cluster.status')
             if [[ $fst =~ "AVAILABLE" ]];then
                 $status="Finished"
-                amb=$(cb cluster describe --name myhdp3cluster | grep -i ambariServerUrl)
-                add_log "$amb"
+                add_log "Cluster creation completed"
+                amb=$(cb cluster describe --name $clusname | grep -i ambariServerUrl)
                 add_log "Cluster status is AVAILABLE"
+                add_log "Ambari Server URL - $amb"
+            fi
+            if [[ $fst =~ "FAILED" ]];then
+                $status="FAILED"
+                exit_script "Cluster creation Failed - check the logs in the Cloudbreak VM using the command cbd logs cloudbreak"
             fi
         fi
     done
 }
 
+cleanup()
+{
 
+}
 
-#start_script
-#install_Cb
-#get_arm
-#register_blueprint
-#create_cluster
-#get_cluster_status
-
+0_start_script
+1_install_cb_jq
