0d076d45615baedbb47de86070845fa1cabdc941
Svenugopal <47510467+svenugopal333@users.noreply.github.com>
2019-05-18 19:11:37 +1000

    Update 1.cbdeployer.sh

--- 1.cbdeployer.sh
+++ 1.cbdeployer.sh
@@ -41,7 +41,7 @@ systemctl status docker
 
 echo "###################################"
 echo "Installing Cloudbreak"
-yum install epel-release -y
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install jq -y
 curl -Ls public-repo-1.hortonworks.com/HDP/cloudbreak/cloudbreak-deployer_2.9.0_$(uname)_x86_64.tgz | sudo tar -xz -C /bin cbd
 
