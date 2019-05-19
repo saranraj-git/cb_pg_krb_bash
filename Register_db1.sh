0ebe2c8608b87f2ea373fd04d60bc3181496878d
Svenugopal <47510467+svenugopal333@users.noreply.github.com>
2019-05-19 21:58:47 +1000

    Update Register_db1.sh

--- Register_db1.sh
+++ Register_db1.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 : '
 This script helps in integrating the Postgres databases (for HDP/HDF clusters) to Cloudbreak:
-It requires the following as input:
-1. Postgres DB server FQDN
-2. DB Username and password
-3. Cloudbreak deployer VM fqdn along with the username and password for Cloudbreak WebUI
+It requires the following parameters in the same order:
+Parameter 1 : Postgres DB - username
+Parameter 2 : Postgres DB - password
+Parameter 3 : CB utility - Artifactory URL to download
+Parameter 4 : Cloudbreak VM IP address from T2
+Parameter 5 : Cloudbreak Web UI - username
+Parameter 6 : Cloudbreak Web UI - Password
+
 '
 # Getting the inputs as parameter for Cloudbreak WebUI and postgres server 
-pgserver=$1     #"postgres.server.com"
-pgusername=$2   #Eg: "cbadmin"
-pgpwd=$3        #some secure pwd from DevOps
-cburl="https://$(ip add | grep 'state UP' -A2 | head -n3 | awk '{print $2}' | cut -f1 -d'/' | tail -n1)"
-cbuser=$4       #Eg: "cbadmin@example.com "
-cbpasswd=$5     #some secure pwd from DevOps
+pgserver=$(ip add | grep 'state UP' -A2 | head -n3 | awk '{print $2}' | cut -f1 -d'/' | tail -n1)
+pgusername=$1   #Eg: "cbadmin or postgres"
+pgpwd=$2        #some secure pwd from DevOps
+cbutilpath=$3    #artifactory url for CB utility
+cbip=$4          
+cb_web_url="https://$cbip"
+cbusrname=$5       #Eg: "cbadmin@example.com "
+cbpwd=$6     #some secure pwd from DevOps
+tstmp=$(date "+%Y%m%d%H%M%S")
+ambaridbname="ambari$tstmp"
+hivedbname="hive$tstmp"
+rangerdbname="ranger$tstmp"
+registrydbname="registry$tstmp"
 
-start_script()
+start_script_0()
 {
     if [ -d "/var/log/hwx/" ]; then
         echo `date "+%Y-%m-%d %H:%M:%S : Script Execution started"` >> /var/log/hwx/register_db.log
@@ -33,29 +44,88 @@ exit_script()
     exit 1
 }
 
-
 # Installing and configuring CB utility 
-install_cb()
+install_cb_1() # CB Configure need to be tested
 {
-    if [[ $(curl -Ls https://s3-us-west-2.amazonaws.com/cb-cli/cb-cli_2.9.0_Linux_x86_64.tgz | sudo tar -xz -C /bin cb && chmod +x /bin/cb) -eq 0 ]];then
-        add_log "CB utility downloaded successfully"
-        if [[ $(cb configure --server $cburl --username $cbuser --password $cbpasswd) -eq 0 ]];then
+    if [[ ! -f /bin/cb ]]; then
+        if [[ $(wget $cbutilpath -O /bin/cb) -eq 0 ]] && [[ $(chmod +x /bin/cb) -eq 0 ]];then
+            add_log "CB utility downloaded successfully"
+            if [[ $(cb configure --server $cb_web_url --username $cbusrname --password $cbpwd) -eq 0 ]] && [[ $(cb blueprint list) -eq 0 ]];then
+                add_log "CB configured succcessfully with Cloudbreak VM"
+            else
+                exit_script "Unable to configure CB Utility with Cloudbreak"
+            fi
+        else
+            exit_script "Failed to Download CB from the internet"
+        fi
+    else
+        add_log "CB file already exists"
+        if [[ $(cb configure --server $cb_web_url --username $cbusername --password $cbpwd) -eq 0 ]] && [[ $(cb cluster list) -eq 0 ]];then
             add_log "CB configured succcessfully with Cloudbreak VM"
         else
             exit_script "Unable to configure CB Utility with Cloudbreak"
         fi
+    fi 
 
+    : '
+
+    add_log "Checking JQ ...."
+    if [[ ! -f /bin/jq ]]; then
+        add_log "JQ not installed on this machine"
+        if [[ $(wget $jqurl -O /bin/jq) ]] && [[ $(chmod +x /bin/jq) ]]; then
+            add_log "Installed JQ version - $(jq --version)"
+        else
+            exit_script "Error installing JQ"
+        fi
     else
-        exit_script "Failed to Download CB from the internet"
+        add_log "JQ already installed - $(jq --version)"
     fi
+    '
+
 }
 
-# Register External Postgres DB created for HDP/HDF clusters with Cloudbreak
-register_db()
+create_db_2()
 {
-    if [[ $(cb database create postgres --name ambaridb --type AMBARI --url jdbc:postgresql://$pgserver:5432/ambari?ssl=true  --db-username $pgusername --db-password $pgpwd) -eq 0 ]]; then
+    if [[ $(sudo -i -u postgres psql -c "CREATE DATABASE $ambaridbname;") -eq 0 ]]; then
+        ambval=$(sudo -i -u postgres psql -c "\l+" | grep "$ambaridbname" | cut -f1 -d"|")
+        [[ $ambval ]] && add_log "$ambaridbname database created successfully" || exit_script "Unable to create/validate $ambaridbname database"
+    else
+        exit_script "Failed to create $ambaridbname database"
+    fi
+
+    if [[ $(sudo -i -u postgres psql -c "CREATE DATABASE $hivedbname;") -eq 0 ]]; then
+        hiveval=$(sudo -i -u postgres psql -c "\l+" | grep "$hivedbname" | cut -f1 -d"|")
+        [[ $hiveval ]] && add_log "$hivedbname database created successfully" || exit_script "Unable to create/validate $hivedbname database"
+    else
+        exit_script "Failed to create $hivedbname database"
+    fi
+
+    if [[ $(sudo -i -u postgres psql -c "CREATE DATABASE $rangerdbname;") -eq 0 ]]; then
+        rangerval=$(sudo -i -u postgres psql -c "\l+" | grep "$rangerdbname" | cut -f1 -d"|")
+        [[ $rangerval ]] && add_log "$rangerdbname database created successfully" || exit_script "Unable to create/validate $rangerdbname database"
+    else
+        exit_script "Failed to create $rangerdbname database"
+    fi
+
+    if [[ $(sudo -i -u postgres psql -c "CREATE DATABASE $registrydbname;") -eq 0 ]]; then
+        registryval=$(sudo -i -u postgres psql -c "\l+" | grep "$registrydbname" | cut -f1 -d"|")
+        [[ $registryval ]] && add_log "$registrydbname database created successfully" || exit_script "Unable to create/validate $registrydbname database"
+    else
+        exit_script "Failed to create $registrydbname database"
+    fi
+    
+}
+# Register External Postgres DB created for HDP/HDF clusters with Cloudbreak
+register_db_3()
+{
+    if [[ $(cb database create postgres --name $ambaridbname --type AMBARI --url jdbc:postgresql://$pgserver:5432/$ambaridbname --db-username $pgusername --db-password $pgpwd) -eq 0 ]]; then
         if [[ $(cb database list --output table | grep AMBARI) -eq 0 ]];then
-            add_log "Ambari DB registered successfully with Cloudbreak"
+            cbambdb=$(cb database list -output table | grep $ambaridbname | cut -f2 -d"|")
+            if [[ $cbambdb ]];then 
+                add_log "Ambari DB - $ambaridbname registered successfully with Cloudbreak"
+            else
+                exit_script "Unable to validate Ambari DB - $ambaridbname from cloudbreak"
+            fi
         else
             exit_script "Failed to register Ambari DB with Cloudbreak"
         fi
@@ -63,9 +133,14 @@ register_db()
         exit_script "Failed to register Ambari DB with Cloudbreak"
     fi
 
-    if [[ $(cb database create postgres --name hivedb --type HIVE --url jdbc:postgresql://$pgserver:5432/hive?ssl=true --db-username $pgusername --db-password $pgpwd) -eq 0 ]];then
+    if [[ $(cb database create postgres --name $hivedbname --type HIVE --url jdbc:postgresql://$pgserver:5432/$hivedbname --db-username $pgusername --db-password $pgpwd) -eq 0 ]];then
         if [[ $(cb database list --output table | grep HIVE) -eq 0 ]];then
-            add_log "Hive DB registered successfully with Cloudbreak"
+            cbhivedb=$(cb database list -output table | grep $hivedbname | cut -f2 -d"|")
+            if [[ $cbhivedb ]];then 
+                add_log "Hive DB - $hivedbname registered successfully with Cloudbreak"
+            else
+                exit_script "Unable to validate Hive db - $hivedbname from cloudbreak"
+            fi
         else
            exit_script "Failed to register Hive DB with Cloudbreak"
         fi 
@@ -73,9 +148,14 @@ register_db()
         exit_script "Failed to register Hive DB with Cloudbreak"
     fi
 
-    if [[ $(cb database create postgres --name rangerdb --type RANGERDB --url jdbc:postgresql://$pgserver:5432/rangerdb?ssl=true --db-username $pgusername --db-password $pgpwd) -eq 0 ]];then
+    if [[ $(cb database create postgres --name $rangerdbname --type RANGERDB --url jdbc:postgresql://$pgserver:5432/$rangerdbname --db-username $pgusername --db-password $pgpwd) -eq 0 ]];then
         if [[ $(cb database list --output table | grep RANGERDB) -eq 0 ]];then
-            add_log "Ranger DB registered successfully with Cloudbreak"
+            cbrangerdb=$(cb database list -output table | grep $rangerdbname | cut -f2 -d"|")
+            if [[ $cbrangerdb ]];then 
+                add_log "Ranger DB - $rangerdbname registered successfully with Cloudbreak"
+            else
+                exit_script "Unable to validate Ranger db - $rangerdbname from cloudbreak"
+            fi
         else
             exit_script "Failed to register Ranger DB with Cloudbreak"
         fi
@@ -83,9 +163,14 @@ register_db()
         exit_script "Failed to register Ranger DB with Cloudbreak"
     fi
     
-    if [[ $(cb database create postgres --name nifiregdb --type REGISTRY --url jdbc:postgresql://$pgserver:5432/registry?ssl=true  --db-username $pgusername --db-password $pgpwd) -eq 0 ]];then
+    if [[ $(cb database create postgres --name nifiregdb --type REGISTRY --url jdbc:postgresql://$pgserver:5432/$registrydbname  --db-username $pgusername --db-password $pgpwd) -eq 0 ]];then
         if [[ $(cb database list --output table | grep REGISTRY) -eq 0 ]];then
-            add_log "Registry DB registered successfully with Cloudbreak"
+            cbregistrydb=$(cb database list -output table | grep $registrydbname | cut -f2 -d"|")
+            if [[ $cbregistrydb ]];then 
+                add_log "Registry DB - $registrydbname registered successfully with Cloudbreak"
+            else
+                exit_script "Unable to validate Registry db - $registrydbname from cloudbreak"
+            fi
         else
             exit_script "Failed to register Registry DB with Cloudbreak"
         fi
@@ -96,11 +181,12 @@ register_db()
 
 
 
-if [ $# -eq 1 ]; then
-start_script
-install_cb
-register_db
-
+if [ $# -eq 6 ]; then
+start_script_0
+install_cb_1
+create_db_2
+register_db_3
 else
-    exit_script "This script requires 1 argument : ./myscript.sh <postgres-server-fqdn>"
+    start_script_0
+    exit_script "This script requires 6 arguments : ./myscript.sh <pg-username> <pg-passwd> <http://artifactory/cb> <cb-vm-ipaddress> <cb-username> <cb-passwd>"
 fi
